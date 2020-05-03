4eaad0a8455ccd3d75148ef87eced1360f4ffe08
1234abc1234-New <s29050898@gmail.com>
2020-05-03 20:09:00 +0800

    abc alpha table

--- app/models.py
+++ app/models.py
@@ -89,3 +89,9 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500), index=True, unique=True)
+    content = db.Column(db.String(99999))
