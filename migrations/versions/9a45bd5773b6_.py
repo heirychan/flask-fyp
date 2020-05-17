1c2aa10201c7e20c4539b503533220911776e709
Raphael-LWH <lauraphael21@gmail.com>
2020-05-17 18:20:36 +0800

    RAP heads

---
+++ migrations/versions/9a45bd5773b6_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 9a45bd5773b6
+Revises: 6b8804edba6e, e37f922a337b
+Create Date: 2020-05-17 18:18:48.703276
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9a45bd5773b6'
+down_revision = ('6b8804edba6e', 'e37f922a337b')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
